1eaf51964893f8a9c21686c72c9d65d6e9c8bbf5
Vladislav Yarmak <vladislav@vm-0.com>
2025-12-04 17:25:30 +0200

    randmap implementation

---
+++ randmap/randmap.go
@@ -0,0 +1,79 @@
+// Package randmap implements key-value map capable of uniform sampling
+// of keys in it.
+package randmap
+
+import (
+	"math/rand/v2"
+)
+
+// RandMap represents key-value map which allows to fetch random key from map,
+// granting equal probability of selection among keys. Time complexity of all
+// operations is O(1) am.
+type RandMap[K comparable, V any] struct {
+	kv map[K]V
+	ik map[int]K
+	ki map[K]int
+}
+
+// Make creates empty map.
+func Make[K comparable, V any]() *RandMap[K, V] {
+	return &RandMap[K, V]{
+		kv: make(map[K]V),
+		ik: make(map[int]K),
+		ki: make(map[K]int),
+	}
+}
+
+// Get retrieves key from map.
+func (m *RandMap[K, V]) Get(key K) (val V, ok bool) {
+	item, ok := m.kv[key]
+	return item, ok
+}
+
+// Set adds or updates key-value pair in map.
+func (m *RandMap[K, V]) Set(key K, item V) {
+	oldLen := len(m.kv)
+	m.kv[key] = item
+	if newLen := len(m.kv); newLen > oldLen {
+		// adding new element and updating indexes
+		m.ik[newLen-1] = key
+		m.ki[key] = newLen - 1
+	}
+}
+
+// Delete removes key from map.
+func (m *RandMap[K, V]) Delete(key K) {
+	deletedIdx, ok := m.ki[key]
+	if !ok {
+		return
+	}
+	oldLen := m.Len()
+
+	delete(m.kv, key)
+	delete(m.ki, key)
+	if deletedIdx == oldLen-1 {
+		delete(m.ik, deletedIdx)
+	} else {
+		relocatedKey := m.ik[oldLen-1]
+		m.ki[relocatedKey] = deletedIdx
+		m.ik[deletedIdx] = relocatedKey
+		delete(m.ik, oldLen-1)
+	}
+}
+
+// GetRandom retrieves uniformly-distributed random key-value pair from map,
+// if it's not empty.
+func (m *RandMap[K, V]) GetRandom() (val V, ok bool) {
+	var emptyV V
+	l := m.Len()
+	if l == 0 {
+		return emptyV, false
+	}
+	item, ok := m.kv[m.ik[rand.IntN(l)]]
+	return item, ok
+}
+
+// Len returns number of key-value pairs in map.
+func (m *RandMap[K, V]) Len() int {
+	return len(m.kv)
+}
